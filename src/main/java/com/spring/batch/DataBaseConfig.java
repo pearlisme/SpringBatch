4b4dc5e6078b9b2a137af99cdd3e988a5136dbf3
Pe@rl <muthubharathi.r@gmail.com>
2018-03-01 11:51:23 +0530

    Added logging support
    Signed-off-by: Pe@rl <muthubharathi.r@gmail.com>

--- src/main/java/com/spring/batch/DataBaseConfig.java
+++ src/main/java/com/spring/batch/DataBaseConfig.java
@@ -2,6 +2,8 @@ package com.spring.batch;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,10 +13,12 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class DataBaseConfig {
 
+	private static final Logger logger = LoggerFactory.getLogger(BatchApp.class);
+
 	@ConfigurationProperties(prefix = "spring.datasource")
-    @Bean
-    @Primary
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
+	@Bean
+	@Primary
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
 }
